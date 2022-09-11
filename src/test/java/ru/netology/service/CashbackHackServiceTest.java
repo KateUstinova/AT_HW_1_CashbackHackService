ca9831543924d9f822ca8e0731cfbfeeb566879e
Екатерина Устинова <dadsda@inbox.ru>
2022-09-11 22:23:43 +0700

    add junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,37 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldLowBoundaryService() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldHighBoundaryService() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldEqualBoundaryService() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+}
